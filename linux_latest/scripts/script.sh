89a31bea8c130d1996deb6913a16140cb6f80a4f
escanoru <44711170+escanoru@users.noreply.github.com>
2023-09-20 13:44:08 +0000

    scripts updated

--- linux_latest/scripts/script.sh
+++ linux_latest/scripts/script.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-sudo yum -y update
-sudo yum install -y httpd
+# Replace the commands below with something would actually need to be run on your setup.
+sudo echo "yum -y update"
+sudo echo "yum install -y httpd"
